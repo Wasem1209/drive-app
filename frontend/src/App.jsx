e9aea2ece242f1e7afa8742bc5210ff157a25833
ChukwuEmeka David <156104693+David-coding-lab@users.noreply.github.com>
2025-11-23 23:00:45 +0000

    added the mobile frame ui for the driver side

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Register from './auth/Register.jsx';
 import Login from "./auth/Login";
+import Driver from './pages/Driver.jsx';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/driver" element={<Driver />} />
     </Routes>
   );
 }
