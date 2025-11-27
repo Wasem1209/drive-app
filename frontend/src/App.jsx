6ad923207602a2b43d1bafea96d454d0c93a5bd6
annoor24 <kabirnuruddin24@gmail.com>
2025-11-27 15:16:33 +0100

    Driver dashboard in progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,9 +34,9 @@ function App() {
       <Route
         path="/dashboard/driver"
         element={
-          <PrivateRoute requiredRole="driver">
+          // <PrivateRoute requiredRole="driver">
             <DriverDashboard />
-          </PrivateRoute>
+          // </PrivateRoute>
         }
       />
 
