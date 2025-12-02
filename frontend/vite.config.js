86fd2e0c3d2b913c94e7c0d1478911ec83590fab
Wasem1209 <philipwasem3@gmail.com>
2025-12-02 05:23:14 +0100

    h

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import NodePolyfills from "rollup-plugin-node-polyfills";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // <-- THIS FIXES blank page in production
+  base: "./",
+  resolve: {
+    alias: {
+      // Force lucid-cardano to use the browser entry
+      "lucid-cardano": path.resolve(
+        // eslint-disable-next-line no-undef
+        __dirname,
+        "node_modules/lucid-cardano/browser/index.js"
+      ),
+    },
+  },
   optimizeDeps: {
     include: ["lucid-cardano/browser"],
   },
   build: {
     rollupOptions: {
-      plugins: [
-        NodePolyfills()
-      ],
+      plugins: [NodePolyfills()],
     },
   },
   define: {
