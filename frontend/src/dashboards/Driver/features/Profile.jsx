2a761d17a15df2be8abe643ed5a9130477d5e38c
Philip Terfa Wasem <philipwasem3@gmail.com>
2025-12-08 01:49:47 +0100

    Update Profile.jsx

--- frontend/src/dashboards/Driver/features/Profile.jsx
+++ frontend/src/dashboards/Driver/features/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Profile.css";
+import PhoneFrame from "../../../components/PhoneFrame";
 
 const Profile = () => {
     const [profile, setProfile] = useState({
