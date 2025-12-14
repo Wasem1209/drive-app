da7aee867335fd486933dffc586a80f8a22806bd
Wasem1209 <philipwasem3@gmail.com>
2025-12-14 06:48:40 +0100

    Added route

--- backend/src/routes/vehicle.routes.ts
+++ backend/src/routes/vehicle.routes.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { registerVehicle } from "../controllers/vehicleController";
+import { mintDriverIdentity, mintVehicleIdentity } from "../controllers/cardano.controller";
 
 const router = express.Router();
 
 router.post("/register", registerVehicle);
+// Mint Vehicle Identity NFT
+router.post("/cardano/vehicle-identity", mintVehicleIdentity);
 
 export default router;
